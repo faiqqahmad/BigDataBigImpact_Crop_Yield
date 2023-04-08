c1c1004402ae0de111a041dc223f7bf1af6cd43c
Noahs212 <33463005+Noahs212@users.noreply.github.com>
2023-04-08 11:25:58 -0400

    moved settigns from bottom to top, got rid of double navbar

--- cropyield/src/App.js
+++ cropyield/src/App.js
@@ -16,6 +16,7 @@ function App() {
       <div className='mid'>
 
         <div className="settings">
+          <Settings/>
           <p1>Some settings</p1>
           <p1>Some settings</p1>
           <p1>Some settings</p1>
@@ -31,24 +32,8 @@ function App() {
         </div>
         
       </div>
-
       <div className='bot'><About/></div>
-      <Navbar/>
-      <Settings/>
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
+      
     </div>
   );
 }
